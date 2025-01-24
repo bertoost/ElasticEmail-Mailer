32d783f2defaf4e2772475c28ddb4dfac7a1dcff
cybd <cybd@users.noreply.github.com>
2025-01-24 22:25:56 +0200

    Fix PHP 8.4 deprecation

--- src/Transport/ElasticEmailSmtpTransport.php
+++ src/Transport/ElasticEmailSmtpTransport.php
@@ -11,8 +11,8 @@ class ElasticEmailSmtpTransport extends EsmtpTransport
     public function __construct(
         string $username,
         string $password,
-        EventDispatcherInterface $dispatcher = null,
-        LoggerInterface $logger = null
+        ?EventDispatcherInterface $dispatcher = null,
+        ?LoggerInterface $logger = null
     ) {
         parent::__construct('smtp.elasticemail.com', 2525, false, $dispatcher, $logger);
 
